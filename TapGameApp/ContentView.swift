a5334ce0022abc7f889a1b29ef08cf4945984ce7
akotetsu <yuanliqu@gmail.com>
2025-03-09 00:32:07 +0900

    Initial Commit

---
+++ TapGameApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TapGameApp
+//
+//  Created by 原里駆 on 2025/03/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TapGameApp/TapGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapGameAppApp.swift
+//  TapGameApp
+//
+//  Created by 原里駆 on 2025/03/09.
+//
+
+import SwiftUI
+
+@main
+struct TapGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
